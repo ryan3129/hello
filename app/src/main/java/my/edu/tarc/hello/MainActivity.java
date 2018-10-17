780b073cad516debac5e5c4e7c78106bfb2f23a1
ryan3129 <ryancwk-wa16@student.tarc.edu.my>
2018-10-17 17:51:12 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/hello/MainActivity.java
@@ -0,0 +1,23 @@
+package my.edu.tarc.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textViewMessage);
+        textViewMessage.setText("Hello Ryan");
+    }
+
+    public void showMessage(View view){
+        textViewMessage.setText("Hello Ryan");
+    }
+}
